c182af334482bc200e3e08e9a3e208c0079673ab
Kevin Funderburg <ksfunderburg@icloud.com>
2020-09-15 01:15:15 -0500

    Updates

---
+++ precache-manifest.aa130e3e1587d1e3a647fc2ab99ad673.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a581f0e0519e7d883334ba747a2e5704",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa02ecfc68436fa4f092",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9b8c801e9e90aa8b5b11",
+    "url": "/static/js/2.a9c449ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a9c449ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa02ecfc68436fa4f092",
+    "url": "/static/js/main.e330d78b.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "81ac99077b05c6512ef937330d79d9ff",
+    "url": "/static/media/profile.81ac9907.jpeg"
+  }
+]);
